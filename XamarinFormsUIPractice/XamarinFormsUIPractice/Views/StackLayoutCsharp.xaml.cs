e8a8fc7c17f127b424c24558cac5932dc442ffa5
shuhey <shuhey.dev@gmail.com>
2019-01-14 23:54:06 +0900

    stacklayoutをちょっと変更

--- XamarinFormsUIPractice/XamarinFormsUIPractice/Views/StackLayoutCsharp.xaml.cs
+++ XamarinFormsUIPractice/XamarinFormsUIPractice/Views/StackLayoutCsharp.xaml.cs
@@ -9,6 +9,7 @@ namespace XamarinFormsUIPractice.Views
             InitializeComponent();
 
             var stackLayout = new StackLayout();
+            stackLayout.Orientation = StackOrientation.Vertical;
             stackLayout.Padding = new Thickness(10, 10, 10, 10);
             stackLayout.Spacing = 5;
             stackLayout.Children.Add(new Label
@@ -23,7 +24,8 @@ namespace XamarinFormsUIPractice.Views
                 FontSize = 30,
                 Text = "Second",
                 //VerticalOptions = LayoutOptions.FillAndExpand,//縦方向に余白を最大限に利用する
-                HeightRequest = 200,
+                HorizontalOptions = LayoutOptions.EndAndExpand,
+                //HeightRequest = 200,
                 BackgroundColor = Color.FromHex("53CF9E"),
             });
             stackLayout.Children.Add(new Label()
@@ -47,6 +49,31 @@ namespace XamarinFormsUIPractice.Views
                 HeightRequest = 500,
                 BackgroundColor = Color.FromHex("EB6379"),
             });
+
+            var stackLayout2=new StackLayout();
+            stackLayout2.Orientation = StackOrientation.Horizontal;
+            stackLayout2.Padding=new Thickness(0);
+            stackLayout2.Spacing = 0;
+            stackLayout2.Children.Add(new Label
+            {
+                Text = "Sixth",
+                WidthRequest = 50,
+                BackgroundColor = Color.Bisque,
+            });
+            stackLayout2.Children.Add(new Label
+            {
+                Text = "Seventh",
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                BackgroundColor = Color.Orange,
+            });
+            stackLayout2.Children.Add(new Label
+            {
+                Text = "Eighth",
+                WidthRequest = 50,
+                BackgroundColor = Color.Aquamarine,
+            });
+
+            stackLayout.Children.Add(stackLayout2);
 
             this.Content = stackLayout;
         }
